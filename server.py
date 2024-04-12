3dd3e4dc98fe48c0ddb18e8837e08c5dbf463031
Sharaddition <90986869+Sharaddition@users.noreply.github.com>
2024-04-12 16:08:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,59 @@
 import asyncio
 import websockets
 
-async def server(websocket, path):
-    async for message in websocket:
-        print(f"Received message: {message}")
-        await websocket.send(f"Echo: {message}")
+print("\033[92m OPEN:       Server")
 
-async def start_server():
-    async with websockets.serve(server, "0.0.0.0", 8989):
-        print('Server: ON')
-        await asyncio.Future()  # Run forever
+raw_audio = None
+state = 'Free'
+async def receive_data(websocket):
+  buffer = b''
+  global state
+  global raw_audio
+  while True:
+    try:
+      async for message in websocket:
+        data = message
+        if data == "EOF":
+            print("End of audio stream")
+        elif isinstance(data, str):
+            # Handle JSON or text data
+            print(f"Received text data: {data}")
+        else:
+            # Chunk audio data
+            buffer += data
+            while len(buffer) >= CHUNK_SIZE:
+                chunk = buffer[:CHUNK_SIZE]
+                buffer = buffer[CHUNK_SIZE:]
+                
+    except websockets.ConnectionClosed:
+      print("WebSocket connection closed.")
 
-asyncio.run(start_server())
+
+async def start_server(port):
+    async with websockets.serve(receive_data, "localhost", port):
+        print("\033[92m OPEN:       Websocket \033[0m")
+        await asyncio.Future()  # Run the server indefinitely
+
+async def send_test_message():
+    uri = "ws://localhost:6969"  # Change to your Docker container's IP if not running locally
+
+    async with websockets.connect(uri) as websocket:
+        # Send a test message
+        message = "Hello, WebSocket!"
+        await websocket.send(message)
+        print(f"Sent: {message}")
+
+async def main():
+    # Choose a port number for your WebSocket server
+    port = 6969
+
+    # Create and start the server coroutine
+    server_task = asyncio.create_task(start_server(port))
+    send_task = asyncio.create_task(send_test_message())
+
+    # Run the event loop in the main thread
+    await asyncio.gather(server_task, send_task)  # Wait for both server and printing to finish
+    print("Server stopped.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
